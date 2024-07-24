0589286e28a08155fcdf4dccbea0ae4666e49509
Greg  Careme <gregor.ten1@icloud.com>
2024-07-24 16:10:15 +0200

    Our Food Menu done

--- JS/second-slider.js
+++
@@ -1,7 +0,0 @@
-var swiper2 = new Swiper(".Swiper2", {
-  direction: "horizontal",
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
--- JS/slider.js
+++ JS/slider.js
@@ -1,9 +1,28 @@
-var swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: true,
+document.addEventListener("DOMContentLoaded", function () {
+  // First Slider
+  var swiper1 = new Swiper(".first-slider", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  // Second Slider
+  var swiper2 = new Swiper(".Swiper2", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // Third Slider
+  var swiper3 = new Swiper(".Swiper3", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
