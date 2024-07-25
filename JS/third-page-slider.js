023c375e8c21ebbbb1a5c7fcae5f11cfae39db3f
Greg  Careme <gregor.ten1@icloud.com>
2024-07-25 05:19:05 +0200

    Room Page finished

---
+++ JS/third-page-slider.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper1 = new Swiper(".first-slider", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+});
