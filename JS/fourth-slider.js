557d73fae0bf3953d130dc127b9802b287d2c5f6
Greg  Careme <gregor.ten1@icloud.com>
2024-07-25 17:28:19 +0200

    Ultimate Room done

---
+++ JS/fourth-slider.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // First Slider
+  var swiper1 = new Swiper(".first-slider", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // Second Slider
+  var swiper2 = new Swiper(".Swiper2", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  // Third Slider
+  var swiper3 = new Swiper(".Swiper3", {
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  // Fourth Slider
+  var swiper4 = new Swiper(".Swiper4", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
